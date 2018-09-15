d7a5b01247ea6b3f0f86b30c3937f0d6618063ef
chenjian <chenjian@innotechx.com>
2018-09-15 17:31:24 +0800

    add 144 icon

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,9 @@
 var cacheName = 'helloWorld';
 
 self.addEventListener('install', function (event) {
+  // 
   event.waitUntil(
+    // self.skipWaiting(); // 无需等待，注册成功就激活
     caches.open(cacheName).then(function (cache) {
       cache.addAll([
         '/sw-sample/js/script.js',
